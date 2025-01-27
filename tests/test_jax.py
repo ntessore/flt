fix JAX gradient tests

tests/test_jax.py
@@ -3,14 +3,16 @@ import jax.numpy as jnp
 import jax.test_util
 import flt
 
+jax.config.update("jax_enable_x64", True)
+
 
 def test_dlt_grads(n, rng):
-    a = rng.uniform(0.0, 1.0, size=n)
+    a = jnp.asarray(rng.uniform(0.0, 1.0, size=n))
     jax.test_util.check_grads(flt.dlt, (a,), 2)
 
 
 def test_idlt_grads(n, rng):
-    a = rng.uniform(0.0, 1.0, size=n)
+    a = jnp.asarray(rng.uniform(0.0, 1.0, size=n))
     jax.test_util.check_grads(flt.idlt, (a,), 2)
 
 